48e7a53bc23e58fd9e5efc501a5a0aab1e607045
james hart <jamesmurphy197197@gmail.com>
2023-02-06 17:20:48 +0000

    Initial Commit.

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+When user arrives, pick random word and show in blanks (underscores; _ _ _ _)
+Ask user to guess a letter
+    Validate that user has entered one single letter
+If the letter is in the word, show the letter in blanked word.
+If the letter is not in the word, flag a 'Fail'
+    User has a maximum number of 'Fails'
+Inform the user: 
+    Maximum 'Fails'
+    'Fails' remaining
+    Letters guessed
+    State of word (eg. A P P _ E)
+"""
