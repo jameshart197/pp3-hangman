0d95fe668108ed0868cc3a9190a7a7b8bc6564e7
james hart <jamesmurphy197197@gmail.com>
2023-02-10 18:19:13 +0000

    [REFAC] Refactor names for shorter lines. Add lower() to user guesses

--- run.py
+++ run.py
@@ -45,7 +45,7 @@ def get_user_guess(guessed_letters):
     """
 
     while True:
-        user_guess = input("Guess a letter: ")
+        user_guess = input("Guess a letter: ").strip().lower()
 
         if not user_guess.isalpha() or len(user_guess) > 1:
             print("Please enter a single letter.")
@@ -60,7 +60,7 @@ def get_user_guess(guessed_letters):
     return user_guess
 
 
-def characters_not_guessed(word, characters):
+def letter_not_guessed(word, characters):
     """
     Defines number of characters not guessed and counts down to 0
     """
@@ -73,23 +73,22 @@ def characters_not_guessed(word, characters):
 
 def run_game(word):
     guessed_letters = []
-    num_incorrect_guesses = 0
+    fail_count = 0
 
     print_blanked_word(word, guessed_letters)
-
-    while (num_incorrect_guesses < MAX_INCORRECT_GUESSES) and (characters_not_guessed(word, guessed_letters) > 0):
+    while (fail_count < MAX_INCORRECT_GUESSES) and (letter_not_guessed(word, guessed_letters) > 0):
 
         user_guess = get_user_guess(guessed_letters)
         if user_guess not in word:
-            num_incorrect_guesses += 1
+            fail_count += 1
 
         guessed_letters.append(user_guess)
 
         print(guessed_letters)
-        characters_not_guessed(word, guessed_letters)
+        letter_not_guessed(word, guessed_letters)
         print_blanked_word(word, guessed_letters)
 
-    if num_incorrect_guesses == MAX_INCORRECT_GUESSES:
+    if fail_count == MAX_INCORRECT_GUESSES:
         print(f"Game over, you ran out of guesses.\n The answer was {word}")
     else:
         print(f"Victory, you guessed the word! The word was {word}")
@@ -105,6 +104,7 @@ def main():
     current_index = 0
     while keep_playing:
         word = get_random_word(current_index)
+        print(word)
         run_game(word)
         play_again = input("Enter 'Y' to play again or anything else to exit:\n").strip().upper()
         if play_again == "Y":
